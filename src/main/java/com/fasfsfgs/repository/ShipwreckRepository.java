cd5bfa330b4619f5601e309f79a981dbed284d24
fasfsfgs <fasfsfgs@gmail.com>
2016-04-02 19:01:03 -0300

    modules 1-4

---
+++ src/main/java/com/fasfsfgs/repository/ShipwreckRepository.java
@@ -0,0 +1,8 @@
+package com.fasfsfgs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fasfsfgs.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+}
